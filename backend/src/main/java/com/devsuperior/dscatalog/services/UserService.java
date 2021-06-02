126cca7be1fd13c978e7025e1c3a0c5f0e13cf75
unknown <thaisamorandini@gmail.com>
2021-06-02 20:12:38 -0300

    User CRUD

---
+++ backend/src/main/java/com/devsuperior/dscatalog/services/UserService.java
@@ -0,0 +1,100 @@
+package com.devsuperior.dscatalog.services;
+
+import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dscatalog.dto.RoleDTO;
+import com.devsuperior.dscatalog.dto.UserDTO;
+import com.devsuperior.dscatalog.dto.UserInsertDTO;
+import com.devsuperior.dscatalog.entities.Role;
+import com.devsuperior.dscatalog.entities.User;
+import com.devsuperior.dscatalog.repositories.RoleRepository;
+import com.devsuperior.dscatalog.repositories.UserRepository;
+import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
+import com.devsuperior.dscatalog.services.exceptions.ResourceEntityNotFoundException;
+
+
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private RoleRepository roleRepository;
+	
+	@Transactional(readOnly = true)
+	public Page<UserDTO> findAllPaged(PageRequest pageRequest){
+		Page<User> list = userRepository.findAll(pageRequest);
+		return list.map(x -> new UserDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public UserDTO findById(Long id) {
+		// TODO Auto-generated method stub
+		Optional<User> obj = userRepository.findById(id);
+		User entity = obj.orElseThrow(() -> new ResourceEntityNotFoundException("Entity not found"));
+		return new UserDTO(entity);
+	}
+	
+	@Transactional
+	public UserDTO insert(UserInsertDTO dto) {
+		User entity = new User();
+		copyDtoToEntity(dto,entity);
+		entity.setPassword(passwordEncoder.encode(dto.getPassword()));
+		entity = userRepository.save(entity);
+		return new UserDTO(entity);
+	}
+	
+	@Transactional
+	public UserDTO update(Long id, UserDTO dto) {
+		try {
+			User entity = userRepository.getOne(id);
+			copyDtoToEntity(dto,entity);
+			entity = userRepository.save(entity);
+			return new UserDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceEntityNotFoundException("Id not found " + id);
+		}
+	}
+
+	public void delete(Long id) {
+		// TODO Auto-generated method stub
+		try {
+			userRepository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			// TODO: handle exception
+			throw new ResourceEntityNotFoundException("Id not found " + id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity Violation");
+		}
+		
+	}
+	
+	private void copyDtoToEntity(UserDTO dto, User entity) {
+		// TODO Auto-generated method stub
+		entity.setFirstName(dto.getFirstName());
+		entity.setLastName(dto.getLastName());
+		entity.setEmail(dto.getEmail());
+		
+		entity.getRoles().clear();
+		for (RoleDTO roleDTO : dto.getRoles()) {
+			Role role = roleRepository.getOne(roleDTO.getId());
+			entity.getRoles().add(role);
+		}
+	}
+}
